77ee64d6079d6ca1126c294b051b7260ef20060b
medaz02 <131758135+medaz02@users.noreply.github.com>
2023-05-23 22:29:48 +0200

    Update myrobot.h

--- myrobot.h
+++ myrobot.h
@@ -2,6 +2,7 @@
 #define MYROBOT_H
 
 #include <QObject>
+//On inclut le module network dans le fichier .pro
 #include <QTcpSocket>
 #include <QAbstractSocket>
 #include <QDebug>
@@ -9,11 +10,19 @@
 #include <QMutex>
 
 class MyRobot : public QObject {
-    Q_OBJECT
+    Q_OBJECT;
 public:
-    explicit MyRobot(QObject *parent = 0);
+    explicit MyRobot(QObject *parent = 0);  //MyTcpClient (ancien nom sur le fichier original)
     void doConnect();
     void disConnect();
+    void move(int cas);
+    void set_vitesse(int valeur);
+    void set_etat(int valeur);
+    void set_manette(bool valeur);
+    int get_vitesse();
+
+    //short Crc16(unsigned char *Adresse_tab , unsigned char Taille_max);
+    qint64 Crc16(QByteArray Adresse_tab , int Taille_max);
     QByteArray DataToSend;
     QByteArray DataReceived;
     QMutex Mutex;
@@ -30,6 +39,10 @@ public slots:
 private:
     QTcpSocket *socket;
     QTimer *TimerEnvoi;
+    int m_vitesse;  //vitesse actuelle
+    int m_etat; //etats : avancer/aller Ã  gauche...
+    //Pour la manette
+    bool m_manette;    //Si true : L'utilisateur utilise la manette
 };
 
 #endif // MYROBOT_H
